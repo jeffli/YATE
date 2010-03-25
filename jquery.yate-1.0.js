1b6106eaef9f20fe52a12061e846901bd62583b3
Jeff Li <jxl0326@gmail.com>
2010-03-25 16:44:31 -0700

    update

--- jquery.yate-1.0.js
+++ jquery.yate-1.0.js
@@ -29,7 +29,6 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
- 
 
 (function($) {
   var _cache = {};
@@ -65,11 +64,11 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
       
       var tpl = unescape($node.html()).replace(/[\r\t\n]/g, " ")
       			.replace(/<y:t\s*remove(.*?)>(.*?)<\/y:t>/g, " ")
-      			.replace(/<table(.*?)y:t_remove(.*?)>(.*?)<\/tbody\s*>/g, " ")
-      			.replace(/<thead(.*?)y:t_remove(.*?)>(.*?)<\/tbody\s*>/g, " ")
-      			.replace(/<tbody(.*?)y:t_remove(.*?)>(.*?)<\/tbody\s*>/g, " ")
-      			.replace(/<tr(.*?)y:t_remove(.*?)>(.*?)<\/tr\s*>/g, " ")
-      			.replace(/<td(.*?)y:t_remove(.*?)>(.*?)<\/td\s*>/g, " ")  ;
+      			.replace(/<table([^>]*?)y:t_remove(.*?)>(.*?)<\/tbody\s*>/g, " ")
+      			.replace(/<thead([^>]*?)y:t_remove(.*?)>(.*?)<\/tbody\s*>/g, " ")
+      			.replace(/<tbody([^>]*?)y:t_remove(.*?)>(.*?)<\/tbody\s*>/g, " ")
+      			.replace(/<tr([^>]*?)y:t_remove(.*?)>(.*?)<\/tr\s*>/g, " ")
+      			.replace(/<td([^>]*?)y:t_remove(.*?)>(.*?)<\/td\s*>/g, " ")  ;
       
       var re = new RegExp ('[<|{]y:t\s*(.*?)(="")?[>|}](.*?)[<|{]\/y:t[>|}]');
       var matches = re.exec (tpl);
